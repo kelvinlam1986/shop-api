62562fe4bb961c345d2ac097cda0f00e435fc003
Minh Lam Su <kelvincoder@gmail.com>
2019-11-25 17:08:20 +0700

    Update model for Supplier

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -14,20 +14,28 @@ namespace ShopApi.Models
         [Column(Order = 1, TypeName = "nvarchar(100)")]
         public string Name { get; set; }
 
-        [Column(Order = 2, TypeName = "nvarchar(50)")]
-        public string CreatedBy { get; set; }
+        [Required]
+        [Column(Order = 2, TypeName = "nvarchar(300)")]
+        public string Address { get; set; }
 
-        [Column(Order = 3)]
-        public DateTime CreatedDate { get; set; }
+        [Required]
+        [Column(Order = 3, TypeName = "varchar(50)")]
+        public string Contact { get; set; }
 
         [Column(Order = 4, TypeName = "nvarchar(50)")]
-        public string UpdatedBy { get; set; }
+        public string CreatedBy { get; set; }
 
         [Column(Order = 5)]
+        public DateTime CreatedDate { get; set; }
+
+        [Column(Order = 6, TypeName = "nvarchar(50)")]
+        public string UpdatedBy { get; set; }
+
+        [Column(Order = 7)]
         public DateTime UpdatedDate { get; set; }
 
         [Required]
-        [Column(Order = 6)]
+        [Column(Order = 8)]
         public int BranchId { get; set; }
 
         [ForeignKey(nameof(BranchId))]
